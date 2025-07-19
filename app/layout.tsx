8cb69e386e7ba9910110663e608130448f2f1252
peterdinis <petter1@pobox.sk>
2025-07-19 19:16:57 +0200

    feat: Working on homepage WIP

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import HeroUiProvider from "@/components/providers/HeroUiProvider";
 import Navigation from "@/components/shared/Navigation";
 
 const geistSans = Geist({
@@ -29,10 +28,8 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <HeroUiProvider>
           <Navigation />
           {children}
-        </HeroUiProvider>
       </body>
     </html>
   );
