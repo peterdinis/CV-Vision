21a5b5dbd0a55912c04cb9270cedf1cdac3aa13e
peterdinis <petter1@pobox.sk>
2025-07-20 21:17:48 +0200

    feat: Change openai model

--- actions/cvActions.ts
+++ actions/cvActions.ts
@@ -19,7 +19,7 @@ export const analyzeAndUploadCVAction = actionClient
             }
 
             const completion = await openai.chat.completions.create({
-                model: 'gpt-4o',
+                model: 'gpt-3.5-turbo',
                 messages: [
                     {
                         role: 'system',
